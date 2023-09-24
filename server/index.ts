4368dc73dac8a3e7ea3e7fc9ecd6fa7381122c9e
shane-jung <71043373+shane-jung@users.noreply.github.com>
2023-09-24 17:00:43 -0500

    Added https to backend

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import fs from 'fs';
+import https from 'https';
 import mongoose from 'mongoose';
 
 import initializeServer from './initializeServer';
@@ -13,4 +15,14 @@ mongoose
 
 const app = initializeServer(router);
 
-app.listen(5000, () => console.log(`Listening on port ${5000}`)); // eslint-disable-line
+https
+    .createServer(
+        {
+            key: fs.readFileSync('server.key'),
+            cert: fs.readFileSync('server.cert'),
+        },
+        app,
+    )
+    .listen(5000, function () {
+        console.log(`Listening on port ${5000}`);
+    });
