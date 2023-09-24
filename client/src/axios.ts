1ad8c2792aa08a8180bae254976f1974b1b2edd6
shane-jung <71043373+shane-jung@users.noreply.github.com>
2023-09-24 14:01:41 -0500

    Added /api prefix to url

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log('CREATING AXIOS');
 console.log(import.meta.env.VITE_API_BASE_URL);
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
     headers: { 'Content-Type': 'application/json' },
 });
 
